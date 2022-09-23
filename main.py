732ea1dc80ab7627c1eea6c0bf45dd7edeb47316
aloster <asket2013@ya.ru>
2022-09-23 17:21:55 +1000

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ import vk
 
 
 def main():
-    cloud = input('Куда выгрузить (google или yandex, q - для выхода): ')
-    if cloud == 'q':
-        exit()
-    id_vk = input('Введите id пользователя VK: ')
-    files = vk.top_vk_photos(id_vk)
-    if cloud == 'yandex':
-        token = input('Введите Ваш токен YandexDisk: ')
-        ya_uploader = yandex_disk.YaUploader(token)
-        ya_uploader.upload(id_vk, files)
-    elif cloud == 'google':
-        secret_file = input('Введите имя файла для Google API OATH2: ')
-        google_uploader = google_drive.GoogleUploader(secret_file)
-        google_uploader.upload(id_vk, files)
+    while True:
+        cloud = input('Куда выгрузить (google или yandex, q - для выхода): ')
+        if cloud == 'q':
+            print('Пока!!!')
+            return
+        elif cloud == 'google' or cloud == 'yandex':
+            id_vk = input('Введите id пользователя VK: ')
+            files = vk.top_vk_photos(id_vk)
+            if cloud == 'yandex':
+                token = input('Введите Ваш токен YandexDisk: ')
+                ya_uploader = yandex_disk.YaUploader(token)
+                ya_uploader.upload(id_vk, files)
+            elif cloud == 'google':
+                secret_file = input('Введите имя файла для Google API OATH2: ')
+                google_uploader = google_drive.GoogleUploader(secret_file)
+                google_uploader.upload(id_vk, files)
+        else:
+            print('Введите доступные команды!')
+            continue
 
 
 if __name__ == "__main__":
